0fb8dc9e5fa4d850fa12fd0eb5ec9110b1560ce9
alinasirlou2020 <1384alinasirlou@gmail.com>
2026-07-23 23:09:34 +0330

    fix RPC url in Wagmi.ts

--- Marketplace-frontend/src/config/wagmi.ts
+++ Marketplace-frontend/src/config/wagmi.ts
@@ -3,14 +3,14 @@ import { http } from 'wagmi'
 import { polygonAmoy } from './chain'
 
 export const wagmiConfig = getDefaultConfig({
-  appName: 'NFT Marketplace',
-  projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID || 'demo',
+  appName: "NFT Marketplace",
+  projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID || "demo",
   chains: [polygonAmoy],
   transports: {
     [polygonAmoy.id]: http(
       import.meta.env.VITE_RPC_URL ||
-        'https://rpc-amoy.polygon.technology',
+        "https://black-morning-leaf.matic-amoy.quiknode.pro/c2187aafe6d9a121628b152731cf5dd3cb3e6b59/",
     ),
   },
   ssr: false,
-})
+});
