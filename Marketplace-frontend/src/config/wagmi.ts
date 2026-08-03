75967dc952c78e0ecfa914305e4da18091c167cb
alinasirlou2020 <1384alinasirlou@gmail.com>
2026-08-03 12:19:53 +0330

    Set walletconnect project ID

--- Marketplace-frontend/src/config/wagmi.ts
+++ Marketplace-frontend/src/config/wagmi.ts
@@ -4,7 +4,9 @@ import { polygonAmoy } from './chain'
 
 export const wagmiConfig = getDefaultConfig({
   appName: "NFT Marketplace",
-  projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID || "demo",
+  projectId:
+    import.meta.env.VITE_WALLETCONNECT_PROJECT_ID ||
+    "542c06313f51345944e5c24809c43754",
   chains: [polygonAmoy],
   transports: {
     [polygonAmoy.id]: http(
